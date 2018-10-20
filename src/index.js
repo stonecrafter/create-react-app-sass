9c24270c9936e62d805a994f33c8cb99f72d4c9c
stonecrafter <kirien.chen@gmail.com>
2018-10-19 22:49:05 -0700

    Add sass support and structure

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './stylesheets/main.css';
+import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
